7b8564a619b42bda9eebe2633ddc574d8367b6b8
Dmitriy Ovchinnikov <ovchinnikov.dk@phystech.edu>
2016-10-23 22:45:29 +0300

    HomeWork#4

---
+++ src/ru/sbt/homework4/CountMapImpl.java
@@ -0,0 +1,64 @@
+package ru.sbt.homework4;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by dmitry on 23.10.16.
+ */
+public class CountMapImpl<T> implements CountMap<T> {
+    private HashMap<T, Integer> map;
+
+    public CountMapImpl() {
+        this.map = new HashMap<>();
+    }
+
+    @Override
+    public void add(T o) {
+        Integer count = map.get(o);
+        if (count == null) {
+            map.put(o, 1);
+        }
+        else {
+            map.put(o, count + 1);
+        }
+    }
+
+    @Override
+    public int getCount(T o) {
+        return map.get(o) != null ? map.get(o) : -1;
+    }
+
+    @Override
+    public int remove(T o) {
+        Integer count = map.get(o);
+        map.remove(o);
+        return count != null ? count : -1;
+    }
+
+    @Override
+    public int size() {
+        return map.size();
+    }
+
+    @Override
+    public void addAll(CountMap source) {
+        for (Object object : source.toMap().entrySet()) {
+            Map.Entry<T, Integer> entry = (Map.Entry<T, Integer>) object;
+            Integer count = this.map.get(entry.getKey());
+            this.map.put(entry.getKey(), count == null ? entry.getValue() : entry.getValue() + count);
+        }
+    }
+
+    @Override
+    public Map toMap() {
+        return this.map;
+    }
+
+    @Override
+    public void toMap(Map destination) {
+        for (Map.Entry<T, Integer> entry : this.map.entrySet()) {
+            map.put(entry.getKey(), entry.getValue());
+        }
+    }
+}
